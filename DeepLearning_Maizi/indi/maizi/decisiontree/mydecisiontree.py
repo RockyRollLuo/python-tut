7a74019de0e31ed9de7ca2b17659aa1a5d9c2bc1
RockyRollLuo <luoqi4110217@hotmail.com>
2018-01-10 22:09:45 +0800

    KNN

--- DeepLearning_Maizi/indi/maizi/decisiontree/mydecisiontree.py
+++ DeepLearning_Maizi/indi/maizi/decisiontree/mydecisiontree.py
@@ -4,68 +4,68 @@
 
 from sklearn.feature_extraction import DictVectorizer
 import csv
-
+import os
 from sklearn import preprocessing
 from sklearn import tree
 from sklearn.externals.six import StringIO
 
+basePath = os.path.abspath(".")
+parentPath = os.path.abspath("..")
 
-#从csv文件中读取数据，并保存到allElectronicsData变量中
-allElectronicsData = open(r'.\..\datasets\AllElectronics.csv','r')
+# 从csv文件中读取数据，并保存到allElectronicsData变量中
+allElectronicsData = open(r'.\datasets\AllElectronics.csv', 'r')
 
 # csv的reader方法按行读取数据
 reader = csv.reader(allElectronicsData)
-#next方法读取到csv文件的第一行数据
+# next方法读取到csv文件的第一行数据
 headers = next(reader)
-#打印第一行数据
+# 打印第一行数据
 print(headers)
 
-
-#建两个list，featureList装特征值，labelList装类别标签
+# 建两个list，featureList装特征值，labelList装类别标签
 featureList = []
 labelList = []
 
-#遍历csv文件的每一行
+# 遍历csv文件的每一行
 for row in reader:
-    #将类别标签加入到labelList中
-    labelList.append(row[len(row)-1])
-    #下面这几步的目的是为了让特征值转化成一种字典的形式，就可以调用sk-learn里面的DictVectorizer，直接将特征的类别值转化成0,1值
+    # 将类别标签加入到labelList中
+    labelList.append(row[len(row) - 1])
+    # 下面这几步的目的是为了让特征值转化成一种字典的形式，就可以调用sk-learn里面的DictVectorizer，直接将特征的类别值转化成0,1值
     rowDict = {}
-    for i in range(1,len(row)-1):
+    for i in range(1, len(row) - 1):
         rowDict[headers[i]] = row[i]
     featureList.append(rowDict)
 print(featureList)
 
-#实例化
+# 实例化
 vec = DictVectorizer()
 dummyX = vec.fit_transform(featureList).toarray()
-print("dummyX:"+str(dummyX))
+print("dummyX:" + str(dummyX))
 print(vec.get_feature_names())
 
 # label的转化，直接用preprocessing的LabelBinarizer方法
 lb = preprocessing.LabelBinarizer()
 dummyY = lb.fit_transform(labelList)
-print("dummyY:"+str(dummyY))
-print("labelList:"+str(labelList))
+print("dummyY:" + str(dummyY))
+print("labelList:" + str(labelList))
 
+# criterion是选择决策树节点的标准，这里是按照“熵”为标准，即ID3算法；默认标准是gini index，即CART算法。
+clf = tree.DecisionTreeClassifier(criterion='entropy')
+clf = clf.fit(dummyX, dummyY)
+print("clf:" + str(clf))
 
-#criterion是选择决策树节点的标准，这里是按照“熵”为标准，即ID3算法；默认标准是gini index，即CART算法。
-clf = tree.DecisionTreeClassifier(criterion = 'entropy')
-clf = clf.fit(dummyX,dummyY)
-print("clf:"+str(clf))
+# 生成dot文件
+with open("allElectronicInformationGainOri.dot", 'w') as f:
+    f = tree.export_graphviz(clf, feature_names=vec.get_feature_names(), out_file=f)
 
-#生成dot文件
-with open("allElectronicInformationGainOri.dot",'w') as f:
-    f = tree.export_graphviz(clf,feature_names = vec.get_feature_names(),out_file = f)
-
-#测试代码，取第1个实例数据，将001->100，即age：youth->middle_aged
-oneRowX = dummyX[0,:]
-print("oneRowX:"+str(oneRowX))
+# 测试代码，取第1个实例数据，将001->100，即age：youth->middle_aged
+oneRowX = dummyX[0, :]
+print("oneRowX:" + str(oneRowX))
 newRowX = oneRowX
 newRowX[0] = 1
 newRowX[2] = 0
-print("newRowX:"+str(newRowX))
+print("newRowX:" + str(newRowX))
 
-#预测代码
+# 预测代码
 predictedY = clf.predict(newRowX)
-print("predictedY:"+str(predictedY))
+print("predictedY:" + str(predictedY))
